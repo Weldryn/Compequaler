93205d9e8f7c5aa0703d02bb21544848ae4a1058
Gaétan Fache <fache.gaetan@gmail.com>
2020-01-26 00:04:11 +0000

    Add IRuntimeHashable

---
+++ Compequaler.Core/Hash/IRuntimeHashable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compequaler.Hash
+{
+	public interface IRuntimeHashable
+	{
+		RuntimeHash Hash(RuntimeHash seed);
+	}
+}
